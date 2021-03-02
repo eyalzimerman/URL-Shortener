084ec14f55aaee3c639849ea1f24b1350d37856b
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-03 00:42:45 +0200

    added URL and DataBase classes

--- api/shorturl.js
+++ api/shorturl.js
@@ -1,8 +1,16 @@
-const { timingSafeEqual } = require("crypto");
+const { DH_UNABLE_TO_CHECK_GENERATOR } = require("constants");
 const express = require("express");
 const fs = require("fs");
+const DataBase = require("../scriptdatabase");
 const router = express.Router();
+router.use(express.json());
+router.use(express.urlencoded());
+const DB = new DataBase();
 
-router.get("/", (request, response) => {});
+router.post("/new", (req, res) => {
+  const { url } = req.body;
+  DB.addUrl(url);
+  res.status(200).json({ original_url: `${url}`, shorturl: "1" });
+});
 
 module.exports = router;
--- scriptdatabase.js
+++ scriptdatabase.js
@@ -1,5 +1,35 @@
 const fs = require("fs");
 
 class DataBase {
-  constructor() {}
+  constructor() {
+    this.urlData = [];
+  }
+
+  addUrl(url) {
+    this.urlData.push(new Url(url));
+    fs.writeFile(
+      "./database/database.json",
+      JSON.stringify(this.urlData, null, 4)
+    ).then((res) => {
+      console.log(this.urlData);
+    });
+  }
+
+  isExist() {}
 }
+
+class Url {
+  constructor(originalUrl) {
+    this.originalUrl = originalUrl;
+    this.shortUrlId = Date.now();
+    this.redirectCount = 0;
+    this.creationDate = this.getSQLDate(new Date());
+  }
+  getSQLDate(date) {
+    return `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  }
+}
+
+module.exports = DataBase;
