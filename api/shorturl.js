a9cd6244d4b8e7fab707f54758cb780455f006c1
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-03 23:18:53 +0200

    added error handling and middlewares

--- api/shorturl.js
+++ api/shorturl.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fsPromise = require("fs/promises");
 const DataBase = require("../scriptdatabase");
+const validUrl = require("valid-url");
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded());
@@ -9,22 +10,35 @@ const DB = new DataBase();
 // Post method
 router.post("/new", checkIfExist, (req, res) => {
   const { url } = req.body;
-  DB.addUrl(url).then((newUrl) => {
-    res
-      .status(200)
-      .json(
-        `original_url: ${newUrl.originalUrl}, short_url: ${newUrl.shortUrlId}`
-      );
-  });
+  if (!validUrl.isUri(url)) {
+    throw new Error("Invalid URL");
+  }
+  DB.addUrl(url)
+    .then((newUrl) => {
+      res
+        .status(200)
+        .json(
+          `original_url: ${newUrl.originalUrl}, short_url: ${newUrl.shortUrlId}`
+        );
+    })
+    .catch((error) => {
+      res.status(400).send(`${error}`);
+    });
 });
 
 //Get method
-router.get("/:shorturl", (req, res) => {
+router.get("/:shorturl", checkUrlFormat, (req, res) => {
   const { shorturl } = req.params;
-  DB.findOriginalUrl(shorturl).then((objUrl) => {
-    objUrl.redirectCount++;
-    res.redirect(`${objUrl.originalUrl}`);
-  });
+  DB.findOriginalUrl(shorturl)
+    .then((objUrl) => {
+      if (!objUrl) {
+        throw new Error("Short Url Is Not Found");
+      }
+      res.redirect(`${objUrl.originalUrl}`);
+    })
+    .catch((error) => {
+      res.status(404).send(`${error}`);
+    });
 });
 
 // Check if Url Is Exist middleware
@@ -41,6 +55,16 @@ function checkIfExist(req, res, next) {
     .catch((e) => {
       next();
     });
+}
+
+function checkUrlFormat(req, res, next) {
+  const { shorturl } = req.params;
+  Number(shorturl);
+  if (shorturl.length !== 13 || /\D/.test(shorturl)) {
+    res.status(400).send("ERROR: Wrong Format");
+    return;
+  }
+  next();
 }
 
 module.exports = router;
