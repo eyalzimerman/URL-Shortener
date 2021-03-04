10959c254f056558123dba344bab2cb4e49d276f
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-04 20:19:52 +0200

    added tests for get method in shorturl file

--- main.test.js
+++ main.test.js
@@ -49,3 +49,43 @@ describe("Post Route Tests", () => {
     expect(DB.urlData.length).not.toBe(DBLength);
   });
 });
+
+// tests for ShortUrl GET route
+describe("ShortUrl GET route", () => {
+  const expectedResultExistUrl = {
+    originalUrl: "https://www.youtube.com/watch?v=tIuU_ra1YmY",
+    shortUrlId: "1614871697604",
+    redirectCount: 6,
+    creationDate: "2021-3-4 17:28:17",
+  };
+
+  const messageNotFound = { message: `${new Error()} Short Url Is Not Found` };
+
+  const messageNotFormat = { message: "ERROR: Wrong Format" };
+
+  it("Should get short url and redirect to original url", async () => {
+    const response = await request(app).get("/api/shorturl/1614871697604");
+    // Is the status code 302
+    expect(response.status).toBe(302);
+
+    expect(response.header.location).toEqual(
+      expectedResultExistUrl.originalUrl
+    );
+  });
+
+  it("Should get short url not in Format and return error 400", async () => {
+    const response = await request(app).get("/api/shorturl/abcdef");
+
+    expect(response.status).toBe(400);
+
+    expect(response.body).toEqual(messageNotFormat);
+  });
+
+  it("Should get short url that does not exist and return status 404 error", async () => {
+    const response = await request(app).get("/api/shorturl/1614871123604");
+
+    expect(response.status).toBe(404);
+
+    expect(response.body).toEqual(messageNotFound);
+  });
+});
